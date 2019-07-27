ecb35cc167bfb6d40017e2732e1618898ef9d036
Yash Desai <ys273139@dal.ca>
2019-07-27 00:41:01 -0300

    Password Hashing using bcrypt

--- Brokers_webservice/api/controllers/BrokerFunctionsBoomController.js
+++ Brokers_webservice/api/controllers/BrokerFunctionsBoomController.js
@@ -4,11 +4,12 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 module.exports = {
-    
-
     formSubmissionBoom: function(req, res) {
+        bcrypt.hash(req.param('Password'), saltRounds, function(err, hashedPassword) 
+        {
         if(req.param('Name') && req.param('Email') && req.param('Password') && req.param('Address') && req.param('City') && req.param('Province') && req.param('Zip') &&  req.param('Phoneno') && req.param('Houseid') &&  req.param('Mortgagevalue') && req.param('CompanyName') ) 
         {
         var params = req.param('Name');
@@ -17,7 +18,7 @@ module.exports = {
                 emp_address: req.param('Address'),
                 emp_phone : req.param('Phoneno'),
                 emp_email: req.param('Email'),
-                emp_password: req.param('Password'),
+                emp_password: hashedPassword,
                 emp_companyName : req.param('CompanyName'),
                 applicationStatus: 'Submitted',
                 emp_city: req.param('City'),
@@ -33,7 +34,8 @@ module.exports = {
 
         else{
             res.badRequest('The request wasnot successful - Bad Request.');
-        }           
+        }   
+    });        
     },
 
       getApplicationStatus: function(req, res)
@@ -48,18 +50,34 @@ module.exports = {
                 
                     console.log(Broker);
                     if(Broker == undefined) { return res.json({"Message": "Wrong Application Number" })}
-                    else if(Broker.emp_password !== req.param('Password')){
+                    bcrypt.compare(req.param('Password'), Broker.emp_password, function(err, valid) 
+                    {
+                        if(valid)
+                        {
+                            console.log("Inside Bcrypt")
+                            var status =  Broker.applicationStatus;
+                            console.log(status);
+                            sails.log.info(status);
+                            return res.send({appstatus:status});
+                        }
+                        else
+                        {
+                            res.json({"Message": "Wrong Password"})   
+                        }
+
+                    });
+                    // else if(Broker.emp_password !== req.param('Password')){
                         
-                        return res.json({"Message" : "Password is wrong"});
+                    //     return res.json({"Message" : "Password is wrong"});
 
-                    }
-                    else if(!!Broker && !!Broker && !!Broker.emp_password && Broker.emp_password == req.param('Password')){
-                        var status =  Broker.applicationStatus;
-                        console.log(status);
-                        sails.log.info(status);
-                        return res.send({appstatus:status});
+                    // }
+                    // else if(!!Broker && !!Broker && !!Broker.emp_password && Broker.emp_password == req.param('Password')){
+                    //     var status =  Broker.applicationStatus;
+                    //     console.log(status);
+                    //     sails.log.info(status);
+                    //     return res.send({appstatus:status});
 
-                    }
+                    // }
                     
                     })
                     .catch({ name: 'UsageError' }, function (err) {
